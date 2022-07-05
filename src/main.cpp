5f7f14389ee9d521ed841591aaff6bf2a30c6880
Tim Baker <tim@gondwana.net.au>
2022-07-05 12:34:30 +1000

    All buttons working

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,188 @@
 #include <Arduino.h>
 #include <ME200S.hpp>
-
+#define debounceTime 200
 void printArray(char array[]);
-#define testbutton 11
-void buttonPressed()
+#define CHECK_BUTTON_PIN 11
+#define AUTO_FOCUS_PIN 8
+#define SET_BUTTON_PIN 5
+#define SHUTTER_COONTROL_BUTTON_PIN 3
+#define ONESHOT_AF_BUTTON_PIN 2
+#define F2_8_BUTTON_PIN A0
+#define F4_0_BUTTON_PIN A1
+#define F5_6_BUTTON_PIN A2
+#define F8_0_BUTTON_PIN A3
+#define F11_BUTTON_PIN  A5
+#define F16_BUTTON_PIN A7
+#define F22_BUTTON_PIN 10
+
+
+
+
+void checkButton()
 {
-  Serial.println("Button Pressed");
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounceTime)
+  {
+    Serial.println("Check Button Pressed");
+  }
+  last_interrupt_time = interrupt_time;
 }
+void autoFocusButton()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounceTime)
+  {
+    Serial.println("Auto Focus Button Pressed");
+  }
+  last_interrupt_time = interrupt_time;
+}
+void setButton()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounceTime)
+  {
+    Serial.println("set Button Pressed");
+  }
+  last_interrupt_time = interrupt_time;
+}
+void shutterControlButton()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounceTime)
+  {
+    Serial.println("Shutter Control Button Pressed");
+  }
+  last_interrupt_time = interrupt_time;
+}
+void oneShotAFButton()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounceTime)
+  {
+    Serial.println("One Shot AF Button Pressed");
+  }
+  last_interrupt_time = interrupt_time;
+}
+void f2_8Button()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounceTime)
+  {
+    Serial.println("F2.8 Button Pressed");
+  }
+  last_interrupt_time = interrupt_time;
+}
+void F4_0Button()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounceTime)
+  {
+    Serial.println("F4.0 Button Pressed");
+  }
+  last_interrupt_time = interrupt_time;
+}
+void F5_6Button()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounceTime)
+  {
+    Serial.println("F5.6 Button Pressed");
+  }
+  last_interrupt_time = interrupt_time;
+}
+void F8_0Button()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounceTime)
+  {
+    Serial.println("F 8.0 Button Pressed");
+  }
+  last_interrupt_time = interrupt_time;
+}
+void F11Button()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounceTime)
+  {
+    Serial.println("F11 Button Pressed");
+  }
+  last_interrupt_time = interrupt_time;
+}
+void F16Button()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounceTime)
+  {
+    Serial.println("F16 Button Pressed");
+  }
+  last_interrupt_time = interrupt_time;
+}
+void F22Button()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounceTime)
+  {
+    Serial.println("F22 Button Pressed");
+  }
+  last_interrupt_time = interrupt_time;
+}
+
+
+
+
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
   delay(1000);
   Serial.println("Sending Status request in 5 seconds");
-  pinMode(testbutton, INPUT_PULLUP);
-  attachInterrupt(testbutton,buttonPressed,CHANGE);
+  pinMode(CHECK_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(CHECK_BUTTON_PIN, checkButton, FALLING);
+
+  pinMode(AUTO_FOCUS_PIN, INPUT_PULLUP);
+  attachInterrupt(AUTO_FOCUS_PIN, autoFocusButton, FALLING);
+
+  pinMode(SET_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(SET_BUTTON_PIN, setButton, FALLING);
+
+  pinMode(SHUTTER_COONTROL_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(SHUTTER_COONTROL_BUTTON_PIN, shutterControlButton, FALLING);
+
+  pinMode(ONESHOT_AF_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(ONESHOT_AF_BUTTON_PIN, oneShotAFButton, FALLING);
+
+  pinMode(F2_8_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(F2_8_BUTTON_PIN, f2_8Button, FALLING);
+
+  pinMode(F4_0_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(F4_0_BUTTON_PIN, F4_0Button, FALLING);
+
+  pinMode(F5_6_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(F5_6_BUTTON_PIN, F5_6Button, FALLING);
+
+  pinMode(F8_0_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(F8_0_BUTTON_PIN, F8_0Button, FALLING);
+
+  pinMode(F11_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(F11_BUTTON_PIN, F11Button, FALLING);
+
+  pinMode(F16_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(F16_BUTTON_PIN, F16Button, FALLING);
+
+  pinMode(F22_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(F22_BUTTON_PIN, F22Button, FALLING);
 }
 
 void loop()
@@ -36,7 +204,6 @@ void loop()
     Serial.println();
     Serial.println("Failed to get camera version");
   }
-
 }
 
 void printArray(char array[])
